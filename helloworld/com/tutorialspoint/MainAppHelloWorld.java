d33ff9e7c6117e7e0b7578ec58992548ffde5eb0
cag2050 <cag2050@163.com>
2017-07-02 19:06:52 +0800

    m

--- helloworld/com/tutorialspoint/MainAppHelloWorld.java
+++ helloworld/com/tutorialspoint/MainAppHelloWorld.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-public class MainApp1 {
+public class MainAppHelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
